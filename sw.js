1a8518fb9803fb270b883144064ef7c9eb3dd912
D-ivan84 <155764953+D-ivan84@users.noreply.github.com>
2026-09-18 22:11:04 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+
+const CACHE_NAME = 'quiz-app-v1.3';
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css', // <-- AGGIUNTO QUI
+  './manifest.json',
+  './M1.csv',
+  './M2.csv',
+  './M3.csv',
+  './M4.csv',
+  './icona.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
